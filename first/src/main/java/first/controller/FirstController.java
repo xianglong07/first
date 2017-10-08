4cb60ad87ba0e50b473a88a4755bbb65f2bbec4a
xianglong07 <luoxianglong07@qq.com>
2017-10-08 16:18:42 +0800

    add first code

---
+++ first/src/main/java/first/controller/FirstController.java
@@ -0,0 +1,15 @@
+package first.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FirstController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello() {
+		return "hello, guys.";
+	}
+}
